021593417d65221ef058a8ac8df956e0d6449d22
shobhit549 <sshobhit1106@gmail.com>
2025-05-08 00:51:35 +0530

    Removed global css and updated readme with correct development link

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen p-24">
       Hello
     </div>
   );
