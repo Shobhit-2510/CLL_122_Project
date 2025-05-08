b533384197e21925bcb91211c8b91985b62222e2
shobhit549 <sshobhit1106@gmail.com>
2025-05-08 13:23:39 +0530

    Added Navbar.js

---
+++ app/components/Navbar.js
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-red-800 py-8 px-4 flex justify-between items-center'>
+     bandar
+    </div>
+  )
+}
+
+export default Navbar
--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
-import Counter from "./components/counter";
 
 export default function Home() {
   return (
