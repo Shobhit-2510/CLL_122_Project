b9df9222998cd2f93cec0f3df93f8f2c1ab63cca
shobhit549 <sshobhit1106@gmail.com>
2025-05-08 13:21:02 +0530

    Added navbar component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Counter from "./components/counter";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-24">
-      Hello
+      <Navbar />
+      Hello My Name is Shobhit
+      <Counter/>
     </div>
   );
 }
