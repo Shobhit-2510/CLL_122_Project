50534d00c51e55c8b39e5c556b32060dd9dbbe9c
shobhit549 <sshobhit1106@gmail.com>
2025-05-09 12:08:04 +0530

    updated errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     output: 'export',               // Enables static HTML export
     basePath: '/CLL_122_Project',    // Required for GitHub Pages
     assetPrefix: '/CLL_122_Project', // Required for correct asset loading
+    trailingSlash: true,           // Optional: Adds a trailing slash to the end of URLs
     images: {
       unoptimized: true,            // Needed if you're using <Image />
     },
